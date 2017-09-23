f05855c12f287af8c85515a4fdf5822a3b98c48c
Sattar Hummatli <settarxan@gmail.com>
2017-09-23 21:56:16 +0400

    first commit

---
+++ first-polymer-app/sc-img.js
@@ -0,0 +1,62 @@
+const template = document.createElement('template');
+template.innerHTML = `
+    <style>
+        :host {
+            display: block;
+        }
+
+        img {
+            width: 100%;
+        }
+    </style>
+`
+
+const io = new IntersectionObserver(entries => {
+    for(const entry of entries) {
+        if(entry.isIntersecting) {
+            entry.target.setAttribute('full', '');
+        }
+    }
+});
+
+class SCImg extends HTMLElement {
+    static get observedAttributes() {
+        return ['full'];
+    }
+
+    constructor() {
+        super();
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    connectedCallback() {
+        io.observe(this);
+    }
+
+    disconnectedCallback() {
+        io.unobserve(this);
+    }
+
+    get full() {
+        return this.hasAttribute('full');
+    }
+
+    get src() {
+        return this.getAttribute('src');
+    }
+
+    attributeChangedCallback() {
+        if(this.loaded) {
+            return;
+        }
+        const img = document.createElement('img');
+        img.src = this.src;
+        img.onload = _ => {
+            this.loaded = true;
+            this.shadowRoot.appendChild(img);
+        } 
+    }
+}
+
+customElements.define('sc-img', SCImg);
